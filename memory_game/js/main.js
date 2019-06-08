dd274a78da721a1eb4b65aa420b608f86864d08d
William Weisberger <bill.weisberger@gmail.com>
2019-06-08 13:00:26 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,20 +4,19 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
+function checkForMatch() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    }
+    else {
+      alert("Sorry, try again.")
+    }
   }
-  else {
-    alert("Sorry, try again.")
-  }
+}
+
+function flipCard(cardNum) {
+  console.log("User flipped " + cards[cardNum]);
+  cardsInPlay.push(cards[cardNum]);
+  checkForMatch();
 }
