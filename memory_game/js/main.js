294b78756fb22a0a55d96ea564c581d9c75b3ea4
William Weisberger <bill.weisberger@gmail.com>
2019-06-09 11:17:17 -0600

    Created objects to store properties of cards

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,28 @@
 
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -16,7 +37,13 @@ function checkForMatch() {
 }
 
 function flipCard(cardNum) {
-  console.log("User flipped " + cards[cardNum]);
-  cardsInPlay.push(cards[cardNum]);
+  let currentCard = cards[cardNum];
+  console.log("User flipped a " + currentCard.rank);
+  cardsInPlay.push(currentCard.rank);
+  console.log("Image: " + currentCard.cardImage);
+  console.log("Suit: " + currentCard.suit);
   checkForMatch();
 }
+
+flipCard(1);
+flipCard(3);
